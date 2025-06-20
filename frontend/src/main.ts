f419ff9f3b4da6a9eddf1b7cd6348c63a82ae4f8
c0d3engr <c0d3engr@gmail.com>
2025-06-20 20:52:43 +0800

    Angular Material setup

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,13 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app';
-import { provideHttpClient } from '@angular/common/http';
-import { provideAnimations } from '@angular/platform-browser/animations';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-import 'zone.js'; // important for Angular change detection
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideHttpClient(),
-    provideAnimations()
-  ]
-}).catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
